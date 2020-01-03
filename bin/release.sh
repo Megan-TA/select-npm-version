ff760922313a5a2a3e5775390d2ec6b1ef3c7e23
chen_huang <huangchen@theduapp.com>
2020-01-03 17:45:31 +0800

    build:

--- bin/release.sh
+++ bin/release.sh
@@ -6,6 +6,7 @@ VERSION=`npx select-version-cli`
 git add -A
 git commit -m "[build] $VERSION"
 npm --no-git-tag-version version $VERSION 
+git add -A
 # --message "[package] $VERSION"
 git commit --amend -m "[release] $VERSION"
 git push -u origin HEAD
